f80ed0deb963153590ce8b4e44b10a456f82a762
poommomo <perlmomo@gmail.com>
2019-11-23 11:26:45 +0700

    Initial Commit

---
+++ FoodHub-KDSTests/FoodHub_KDSTests.swift
@@ -0,0 +1,34 @@
+//
+//  FoodHub_KDSTests.swift
+//  FoodHub-KDSTests
+//
+//  Created by Poom Penghiran on 23/11/2562 BE.
+//  Copyright © 2562 Poom Penghiran. All rights reserved.
+//
+
+import XCTest
+@testable import FoodHub_KDS
+
+class FoodHub_KDSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
